a6a16d6592200879aedb72ec7c745c7e5944a10d
jmsgnnr <jmsgnnr@gmail.com>
2021-04-01 12:11:57 -0600

    testing es5 syntax pre babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,3 +2,12 @@ console.log('App is running!');
 
 
 // JSX - JAVASCRIPT XML
+// const test = <p>Testing JSX TESTING JSX!</p>;
+var test = React.createElement(
+    "h1",
+    { id: "someid" },
+    "Something New"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(test, appRoot);
